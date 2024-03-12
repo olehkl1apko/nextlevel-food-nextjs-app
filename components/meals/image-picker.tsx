78a6a5e361f03a932edc58e3159129b40881d5a0
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-12 09:38:00 +0200

    ADD: handling form data

--- components/meals/image-picker.tsx
+++ components/meals/image-picker.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { useRef } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+import Image from "next/image";
 
 import classes from "./image-picker.module.css";
 
 interface Props {
-  label?: string;
-  name?: string;
+  label: string;
+  name: string;
 }
 
 export default function ImagePicker({ label, name }: Props) {
-  const imageInput = useRef<HTMLInputElement>();
+  const [pickedImage, setPickedImage] = useState<string | ArrayBuffer | null>();
+  const imageInput = useRef<HTMLInputElement | null>(null);
 
   function handlePickClick() {
     if (imageInput.current) {
@@ -18,10 +20,38 @@ export default function ImagePicker({ label, name }: Props) {
     }
   }
 
+  function handleImageChange(event: ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {typeof pickedImage === "string" && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+              sizes="(max-width: 768px) 100vw, 50vw"
+            />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -29,6 +59,8 @@ export default function ImagePicker({ label, name }: Props) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
